ce8b436f1739d0a9999dfbb83a5e75d6fcbe249b
Saurabh Pujari <saurabhpuj99@gmail.com>
2021-05-11 20:10:49 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,43 @@
+"""
+Elara
+--------
+Elara DB is an easy to use key-value storage for your python projects!
+
+Installation :
+```````````````````
+::
+
+    $ pip install elara   
+    
+Key Features :
+```````````````
+* Offers two modes of execution - normal and secure - exe_secure() generates a key file and encrypts the key-value storage for additional security.
+* Perform various operations on strings, lists and dictionaries.
+* Choose between manual commits after performing operations in-memory or automatically commit every change into the storage.
+* Includes methods to export certain keys or the entire storage.
+* Based on python's in-built json module for easy manipulation and access.
+* Takes inspiration from pickleDB and Redis.
+    
+Links
+`````
+* `Documentation <https://github.com/saurabh0719/elara#readme>`_
+* `pypi <https://pypi.org/project/elara/>`_
+* `Github repository <https://github.com/saurabh0719/elara>`_
+
+"""
 
 from distutils.core import setup
-
-# read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
     
 setup(
   name = 'elara',         
   packages = ['elara'],   
-  version = '0.1.2',      
+  version = '0.1.3',      
   license='three-clause BSD',        
   description = 'Elara DB is an easy to use key-value storage for your python projects!',   
-  long_description = long_description,
-  long_description_content_type = 'text/markdown',
-  author = 'Saurabh Pujari',                   #
+  long_description = __doc__,
+  author = 'Saurabh Pujari',                   
   author_email = 'saurabhpuj99@gmail.com',      
-  url = 'https://github.com/saurabh0719/elara',   
+  url = 'https://github.com/saurabh0719/elara',  
   keywords = [
     'database',
     'key-value',
