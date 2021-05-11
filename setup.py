28da8a8cba56b7dea74d9e6ab80f5c81a4d7c0d1
Saurabh Pujari <saurabhpuj99@gmail.com>
2021-05-11 19:35:39 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 
 from distutils.core import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
 setup(
   name = 'elara',         
   packages = ['elara'],   
-  version = '0.1.0',      
+  version = '0.1.2',      
   license='three-clause BSD',        
   description = 'Elara DB is an easy to use key-value storage for your python projects!',   
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
   author = 'Saurabh Pujari',                   #
   author_email = 'saurabhpuj99@gmail.com',      
   url = 'https://github.com/saurabh0719/elara',   
