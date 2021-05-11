ddb0a0ec48ddde5641b9561767ab142f40388605
Saurabh Pujari <saurabhpuj99@gmail.com>
2021-05-11 19:15:29 +0530

    Added set up tools

---
+++ setup.py
@@ -0,0 +1,31 @@
+
+from distutils.core import setup
+setup(
+  name = 'elara',         
+  packages = ['elara'],   
+  version = '0.1.0',      
+  license='three-clause BSD',        
+  description = 'Elara DB is an easy to use key-value storage for your python projects!',   
+  author = 'Saurabh Pujari',                   #
+  author_email = 'saurabhpuj99@gmail.com',      
+  url = 'https://github.com/saurabh0719/elara',   
+  keywords = [
+    'database',
+    'key-value',
+    'storage',
+    'file storage',
+    'json storage',
+    'json database'
+    'key-value database'  
+    ],   # Keywords that define your package best
+  install_requires=[
+    'cryptography'
+    ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Database',
+    'License :: OSI Approved :: BSD License',   
+    'Programming Language :: Python'
+  ],
+)
