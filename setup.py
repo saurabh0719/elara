21f8c51971ca3d9059738ad315a7b2d44cb44163
Saurabh Pujari <saurabhpuj99@gmail.com>
2021-05-12 12:32:32 +0530

    updated README, README.rst, v0.2.0

--- setup.py
+++ setup.py
@@ -1,40 +1,15 @@
-"""
-Elara
---------
-Elara DB is an easy to use key-value storage for your python projects!
-
-Installation :
-```````````````````
-::
-
-    $ pip install elara   
-    
-Key Features :
-```````````````
-* Offers two modes of execution - normal and secure - exe_secure() generates a key file and encrypts the key-value storage for additional security.
-* Perform various operations on strings, lists and dictionaries.
-* Choose between manual commits after performing operations in-memory or automatically commit every change into the storage.
-* Includes methods to export certain keys or the entire storage.
-* Based on python's in-built json module for easy manipulation and access.
-* Takes inspiration from pickleDB and Redis.
-    
-Links
-`````
-* `Documentation <https://github.com/saurabh0719/elara#readme>`_
-* `pypi <https://pypi.org/project/elara/>`_
-* `Github repository <https://github.com/saurabh0719/elara>`_
-
-"""
-
 from distutils.core import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
     
 setup(
   name = 'elara',         
   packages = ['elara'],   
-  version = '0.1.3',      
+  version = '0.2.0',      
   license='three-clause BSD',        
-  description = 'Elara DB is an easy to use key-value storage for your python projects!',   
-  long_description = __doc__,
+  description = 'Elara DB is an easy to use, key-value database written for python that can also be used as a fast in-memory cache. Includes various methods to manipulate data structures in-memory, secure database files and export data.',   
+  long_description = long_description,
   author = 'Saurabh Pujari',                   
   author_email = 'saurabhpuj99@gmail.com',      
   url = 'https://github.com/saurabh0719/elara',  
@@ -51,7 +26,7 @@ setup(
     'cryptography'
     ],
   classifiers=[
-    'Development Status :: 4 - Beta',      
+    'Development Status :: Beta',      
     'Intended Audience :: Developers',      
     'Topic :: Database',
     'License :: OSI Approved :: BSD License',   
