a7ae11ae015b9bae8eaafedfa96443021f7c7966
Saurabh Pujari <saurabhpuj99@gmail.com>
2021-05-13 05:01:44 +0530

    formatted docstring to block comment

--- elara/__init__.py
+++ elara/__init__.py
@@ -1,9 +1,9 @@
-'''
+"""
 Copyright (c) 2021, Saurabh Pujari
 All rights reserved.
 
 This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this source tree.
-'''
+"""
 from .elara import Elara
 from .elarautil import Util
 from .lru import LRU
--- elara/elarautil.py
+++ elara/elarautil.py
@@ -1,9 +1,9 @@
-'''
+"""
 Copyright (c) 2021, Saurabh Pujari
 All rights reserved.
 
 This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this source tree.
-'''
+"""
 
 from cryptography.fernet import Fernet
 import json
