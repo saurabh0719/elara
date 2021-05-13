c5fe1d31c8d6cc36004a11cb4e0d87594b076abc
Matheus Xavier <mxavier+git@rootcert.net>
2021-05-13 08:28:38 -0300

    Move storage format over to binary, this change allows the addition of: Checksums, File format versioning and feature flags i.e: encryption, this change also makes the db files way more compact.
    Other changes:
    tighten except clauses in Util class, change some names to snake case

--- elara/exceptions.py
+++ elara/exceptions.py
@@ -5,6 +5,7 @@ All rights reserved.
 This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this source tree.
 """
 
+
 # Add all custom exception classes here
 
 
@@ -24,3 +25,11 @@ class FileKeyError(Exception):
 
     def __str__(self):
         return f"Error -> {self.message}"
+
+
+class LoadChecksumError(Exception):
+    pass
+
+
+class LoadIncompatibleDB(Exception):
+    pass
--- setup.py
+++ setup.py
@@ -1,39 +1,36 @@
 from distutils.core import setup
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     long_description = f.read()
-    
+
 setup(
-  name = 'elara',         
-  packages = ['elara'],   
-  version = '0.3.0',      
-  license='three-clause BSD',        
-  description = 'Elara DB is an easy to use, lightweight NoSQL database written for python that can also be used as a fast in-memory cache for JSON-serializable data. Includes various methods to manipulate data structures in-memory, secure database files and export data.',   
-  long_description = long_description,
-  author = 'Saurabh Pujari',                   
-  author_email = 'saurabhpuj99@gmail.com',      
-  url = 'https://github.com/saurabh0719/elara',  
-  keywords = [
-    'database',
-    'key-value',
-    'storage',
-    'file storage',
-    'json storage',
-    'json database',
-    'key-value database' ,
-    'nosql',
-    'nosql database' 
-    'cache',
-    'file cache'
-    ],   
-  install_requires=[
-    'cryptography'
+    name="elara",
+    packages=["elara"],
+    version="0.3.0",
+    license="three-clause BSD",
+    description="Elara DB is an easy to use, lightweight NoSQL database written for python that can also be used as a fast in-memory cache for JSON-serializable data. Includes various methods to manipulate data structures in-memory, secure database files and export data.",
+    long_description=long_description,
+    author="Saurabh Pujari",
+    author_email="saurabhpuj99@gmail.com",
+    url="https://github.com/saurabh0719/elara",
+    keywords=[
+        "database",
+        "key-value",
+        "storage",
+        "file storage",
+        "json storage",
+        "json database",
+        "key-value database",
+        "nosql",
+        "nosql database" "cache",
+        "file cache",
     ],
-  classifiers=[
-    'Development Status :: 4 - Beta',      
-    'Intended Audience :: Developers',      
-    'Topic :: Database',
-    'License :: OSI Approved :: BSD License',   
-    'Programming Language :: Python'
-  ],
+    install_requires=["cryptography", "msgpack"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Database",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+    ],
 )
